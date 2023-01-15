984ac0ee4a80ba003432cac92afe41a55bc55c69
Vadym Repetskyi <vadym.repetskyi@gmail.com>
2023-01-15 12:43:42 +0100

    Zoom WIP

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,25 @@
 package main.java;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import main.java.simulation.Simulation;
 import main.java.simulation.SimulationBuilder;
 import main.java.simulation.entities.animals.Species;
 import main.java.simulation.state.Vector2D;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 
 public class App extends Application {
 	private static Simulation simulation;
@@ -16,7 +27,7 @@ public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Vector2D mapDimensions = new Vector2D(48, 27);
+			Vector2D mapDimensions = new Vector2D(32, 18);
 			SimulationBuilder simulationBuilder = new SimulationBuilder(mapDimensions);
 			Species lion = new Species("Lion", 5, 3);
 			simulationBuilder.addPredatorPhenotype(lion, new Vector2D(16, 16));
@@ -27,11 +38,11 @@ public class App extends Application {
 			primaryStage.setScene(new Scene(root, 800, 600));
 			primaryStage.show();
 
-			Parent list = FXMLLoader.load(getClass().getResource("../resources/views/list.fxml"));
-			Stage stage = new Stage();
-			stage.setTitle("Predators");
-			stage.setScene(new Scene(list, 250, 350));
-			stage.show();
+//			Parent list = FXMLLoader.load(getClass().getResource("../resources/views/list.fxml"));
+//			Stage stage = new Stage();
+//			stage.setTitle("Predators");
+//			stage.setScene(new Scene(list, 250, 350));
+//			stage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,5 +1,65 @@
 package main.java.controllers;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ScrollEvent;
+import main.java.App;
+import main.java.simulation.state.Map;
+import main.java.simulation.state.Tile;
+
+public class MainController implements Initializable {
+	private final Integer tileSize = 128;
+	
+	@FXML
+	private ScrollPane zoomScrollPane;
+
+	@FXML
+	private Group zoomContent;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		zoomScrollPane.addEventFilter(ScrollEvent.SCROLL, new EventHandler<ScrollEvent>() {
+			@Override
+			public void handle(ScrollEvent e) {
+				zoomContent.setScaleX(Math.max(0.1, Math.min(zoomContent.getScaleX() + e.getDeltaY() * 0.01, 1)));
+				zoomContent.setScaleY(zoomContent.getScaleX());
+				e.consume();
+			}
+		});
+		
+		Map map = App.getSimulation().getMap();
+		
+		ObservableList<Node> renderedTiles = zoomContent.getChildren();
+
+		Integer columnIndex = 0;
+		for (Tile[] column : map.getTiles()) {
+			Integer xTranslation = columnIndex * tileSize;
+			
+			Integer rowIndex = 0;
+			for (Tile tile : column) {
+				Integer yTranslation = rowIndex * tileSize;
+				
+				Label button = new Label(columnIndex.toString() + rowIndex.toString());
+				button.setPrefWidth(tileSize);
+				button.setPrefHeight(tileSize);
+				button.setTranslateX(xTranslation);
+				button.setTranslateY(yTranslation);
+				
+				renderedTiles.add(button);
+				
+				rowIndex++;
+			}
+			
+			columnIndex++;
+		}
+	}
 }
